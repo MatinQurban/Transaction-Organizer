39b54759bfd6633c6e87bb7c42b533185274e362
MatinQurban <matinqurban@gmail.com>
2024-06-17 02:01:59 -0700

    connect to google function

--- connectToGoogle.py
+++ connectToGoogle.py
@@ -1,2 +1,49 @@
-def connectToGoogle():
-    pass
+# Description: This file is used to connect to the google api
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def connectToGoogle(token_path: str, credentials_path: str, SCOPES: list):
+    '''
+    Args:
+    token_path: str - the path to the token file 
+    credentials_path: str - the path to the credentials file
+    (name only is alright if the files are in the same directory as the script)
+    SCOPES: list - the scopes to use for the google api
+
+    Returns:
+    service: googleapiclient.discovery.Resource - the service to use for the google api
+    '''
+    
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_path, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_path, "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build("gmail", "v1", credentials=creds)
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f"An error occurred: {error}")
+
+    return service
