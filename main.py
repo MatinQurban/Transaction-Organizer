b5a3dfc680548aa53c4b3bf656f83ae310474119
MatinQurban <matinqurban@gmail.com>
2024-06-17 21:50:19 -0700

    updated main from quickstart.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-pass
+import base64
+
+from connectToGoogle import connectToGoogle
+from getEmails import getEmails
+from getRawText import getRawText
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+
+def main():
+  """
+  Shows basic usage of the Gmail API.
+  Lists the user's Gmail labels.
+  """
+  try:
+    # Call the Gmail API
+    service = connectToGoogle("token1.json", "credentials.json", SCOPES)
+    user = service.users()
+    
+    messages = getEmails(user)
+
+    raw_emails = getRawText(user, messages)
+    #print("Raw emails: ", raw_emails[0])
+
+  except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+  main()
